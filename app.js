28f0f085e42d312fd6390e74643c67e37dcdfaf9
Rahma Al-Wadhahi <61970868+Rahma-AlWadhahi@users.noreply.github.com>
2021-02-18 10:42:55 +0400

    style: :art:

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  console.log("hey from root path!")
-  res.json({message: 'Hello World!', status: 200})
-})
+app.get("/", (req, res) => {
+  console.log("hey from root path!");
+  res.json({ message: "Hello World!", status: 200 });
+});
 
-app.get('/books', (req, res) => {
-    res.json({message: 'Hello from the book path!'})
-  })
+app.get("/books", (req, res) => {
+  res.json({ message: "Hello from the book path!" });
+});
 
-app.get('/authors', (req, res) => {
-    res.json({message: 'Hello from the authors path!'})
-  })
+app.get("/authors", (req, res) => {
+  res.json({ message: "Hello from the authors path!" });
+});
 
-app.get('/publishers', (req, res) => {
-    res.json({message: 'Hello from the publishers path!'})
-  })
+app.get("/publishers", (req, res) => {
+  res.json({ message: "Hello from the publishers path!" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});

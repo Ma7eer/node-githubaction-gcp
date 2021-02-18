c4a997bd1273eeb7119034a3e677452cbe6d650a
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2021-02-18 12:06:25 +0400

    passing test

--- app.test.js
+++ app.test.js
@@ -2,7 +2,7 @@ describe("/sample test", () => {
   it("should test that true === true", () => {
     expect(true).toBe(true);
   });
-  it("should test that true !== true", () => {
-    expect(true).not.toBe(true);
+  it("should test that true !== false", () => {
+    expect(true).not.toBe(false);
   });
 });

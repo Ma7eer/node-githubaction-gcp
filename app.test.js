6f9d876ba758f696594051cea314095d075cbd45
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2021-02-18 12:04:30 +0400

    failing test

--- app.test.js
+++ app.test.js
@@ -2,4 +2,7 @@ describe("/sample test", () => {
   it("should test that true === true", () => {
     expect(true).toBe(true);
   });
+  it("should test that true !== true", () => {
+    expect(true).not.toBe(true);
+  });
 });

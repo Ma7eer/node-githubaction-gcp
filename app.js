5644e34186b4994cf8caadc9ae903312a917591c
Maymoona Al Balushi <maymoona.al.boloshi@gmail.com>
2021-02-18 15:46:05 +0400

    uglify code

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  console.log("hey from root path!");
-  res.json({ message: "Hello Deka!", status: 200 });
-});
+app.get("/",      (req, res) => {
+           console.log("hey from root path!");
+  res.json(
+    { message: "Hello Deka!", status: 200 });
+}
+);
 
 app.get("/books", (req, res) => {
   res.json({ message: "Hello from the book path!" });

a4f7d2a21115b22167383ba98184a6b7f27d6bd6
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2021-02-18 11:42:47 +0400

    formating code

--- app.test.js
+++ app.test.js
@@ -4,17 +4,16 @@ const assert = require('assert')
 
 
 describe('/sample test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true)
-    })
-    // it('should return successfully', () => {
-    //     request(app)
-    //     .get('/books')
-    //     .expect(200)
-    //     .expect((res) => {
-    //         assert.equal(res.body['hello', 'world'])
-    //     })
-    //     expect(true).toBe(true)
-    //   })
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
   })
-  
+  // it('should return successfully', () => {
+  //     request(app)
+  //     .get('/books')
+  //     .expect(200)
+  //     .expect((res) => {
+  //         assert.equal(res.body['hello', 'world'])
+  //     })
+  //     expect(true).toBe(true)
+  //   })
+})
